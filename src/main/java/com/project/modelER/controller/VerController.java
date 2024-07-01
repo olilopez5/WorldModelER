986a18265770638e5d1cfe08b1f1e9c7534d70f4
olivia <olilopez5@gmail.com>
2024-07-01 09:38:05 +0200

    cambios

---
+++ src/main/java/com/project/modelER/controller/VerController.java
@@ -0,0 +1,14 @@
+package com.project.modelER.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ver")
+public class VerController {
+	public static final Logger log = LoggerFactory.getLogger(CrearController.class);
+	
+
+}
